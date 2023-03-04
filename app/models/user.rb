00b9fa00d18a18765d073722a5b0ce2f9dc6dbca
Michael Garbutt michael.garbutt@travelperk.com <michael.garbutt@travelperk.com>
2023-03-04 12:40:38 +0000

    added validations for booking office and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  has_many :offices
+  has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
