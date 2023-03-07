6ce0aaf9dc2c5d5c05257a647a0a02b1d2577844
Babajide Muritala <jide142000@yahoo.com>
2023-03-07 13:50:56 +0100

    connect stimulus controller

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,5 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="map"
 export default class extends Controller {
   connect() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamlkZS1tdXJpdGFsYSIsImEiOiJjbGV4eXV6YmwwaTg2M3hwNDBscnhwMWF6In0.6dCNpQr0FVCgmeW52OrNJQ';
+    const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+    });
   }
 }
