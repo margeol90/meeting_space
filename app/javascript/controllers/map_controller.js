bcb8d36988c54fec479a9e64fc1edf5c6a080046
Babajide Muritala <jide142000@yahoo.com>
2023-03-07 15:07:19 +0100

    add markers to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,11 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = { apiKey: String }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    const map = new mapboxgl.Map({
-    container: this.element,
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
     });
+    this.#addMarkersToMap
+  }
+
+  #addMarkersToMap() {
+    // Create a new marker, set the longitude and latitude, and add it to the map.
+    new mapboxgl.Marker()
+      .setLngLat([-65.017, -16.457])
+      .addTo(this.map);
   }
 }
