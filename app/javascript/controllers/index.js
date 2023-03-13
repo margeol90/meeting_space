3ea7b609b4fc586fba4bd40c3930732989154893
Francisco Lago <francisco.lago@gmail.com>
2023-03-13 16:18:10 +0100

    OK read-more

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,16 +2,21 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import FlatpickrController from "./flatpickr_controller"
-application.register("flatpickr", FlatpickrController)
+import FlatpickrController from "./flatpickr_controller";
+application.register("flatpickr", FlatpickrController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import MapController from "./map_controller"
-application.register("map", MapController)
+import MapController from "./map_controller";
+application.register("map", MapController);
 
-import StarRatingController from "./star_rating_controller"
-application.register("star-rating", StarRatingController)
+import { Application } from "@hotwired/stimulus";
+import ReadMore from "stimulus-read-more";
+
+application.register("read-more", ReadMore);
+
+import StarRatingController from "./star_rating_controller";
+application.register("star-rating", StarRatingController);
