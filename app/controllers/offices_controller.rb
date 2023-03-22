b2bc76cfa12524f2944f3fdbfb71e7929e21ad27
Francisco Lago <francisco.lago@gmail.com>
2023-03-22 22:07:21 +0100

    Applying the stimulus controler that sent me Ines, but do not know how to put it to work

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -89,6 +89,7 @@ class OfficesController < ApplicationController
                                    :description,
                                    :max_capacity,
                                    :photo,
+                                   facilities: [],
                                    # IF WE USE QUANTITIES USE THIS ONE
                                    office_facilities_attributes: %i[_destroy facility_id quantity])
     # IF WE DON'T WANT QUANTITIES USE:
