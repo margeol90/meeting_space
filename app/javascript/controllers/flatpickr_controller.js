efae76b2c87170126bbc7f5be097ba4a5fe1208e
Francisco Lago <francisco.lago@gmail.com>
2023-03-22 17:55:20 +0100

    Mixed both codes in flatpickr (David's and ours)

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -10,57 +10,62 @@ export default class extends Controller {
     flatpickr(this.startDateTarget, {});
     flatpickr(this.endDateTarget, {});
 
+    const newBookingForm = document.getElementsByClassName("new_booking")[0];
+    const editBookingForm = document.getElementsByClassName("edit_booking")[0];
+    const bookingForm = newBookingForm ? newBookingForm : editBookingForm;
+
+    console.log(bookingForm);
+
+    if (bookingForm) {
+      const picker = flatpickr("#start_date", {
+        plugins: [rangePlugin({ input: "#end_date" })],
+        minDate: "today",
+        // calendar is always visible with below:
+        inline: true,
+        disable: JSON.parse(bookingForm.dataset.unavailableDates),
+      });
+    }
+
     const startDateInput = document.getElementById("booking_start_date");
     const endDateInput = document.getElementById("booking_end_date");
-    if (startDateInput) {
-      const unavailableDates = JSON.parse(
-        document.querySelector("#office-booking-dates").dataset.unavailable
-      );
-      const picker = flatpickr(".range_start", {
-        plugins: [rangePlugin({ input: ".range_end" })],
-        minDate: "today",
-        inline: true,
-        disable: unavailableDates,
+    const officePricePerDay = document.getElementById(
+      "office-price-per-day"
+    ).innerText;
+    const totalDays = document.getElementById("total-days");
+    const stringDays = document.getElementById("string-days");
+    const totalCostElement = document.getElementById("total-cost");
+    const submitButton = document.getElementById("submit_button");
+
+    const dynamicPrice = () => {
+      let dateDiffInMilliseconds =
+        new Date(endDateInput.value) - new Date(startDateInput.value);
+      let nbrOfDays = dateDiffInMilliseconds / 86400000 + 1;
+      if (startDateInput.value && endDateInput.value) {
+        totalDays.innerText = nbrOfDays;
+        stringDays.innerText = nbrOfDays === 1 ? " day" : " days";
+        totalCostElement.innerText = nbrOfDays * officePricePerDay;
+        submitButton.disabled = null;
+      } else {
+        submitButton.disabled = "disabled";
+      }
+    };
+
+    [startDateInput, endDateInput].forEach((date) => {
+      date.addEventListener("change", (event) => {
+        dynamicPrice();
       });
-      const officePricePerDay = document.getElementById(
-        "office-price-per-day"
-      ).innerText;
-      const totalDays = document.getElementById("total-days");
-      const stringDays = document.getElementById("string-days");
-      const totalCostElement = document.getElementById("total-cost");
-      const submitButton = document.getElementById("submit_button");
+    });
 
-      const dynamicPrice = () => {
-        let dateDiffInMilliseconds =
-          new Date(endDateInput.value) - new Date(startDateInput.value);
-        let nbrOfDays = dateDiffInMilliseconds / 86400000 + 1;
-        if (startDateInput.value && endDateInput.value) {
-          totalDays.innerText = nbrOfDays;
-          stringDays.innerText = nbrOfDays === 1 ? " day" : " days";
-          totalCostElement.innerText = nbrOfDays * officePricePerDay;
-          submitButton.disabled = null;
-        } else {
-          submitButton.disabled = "disabled";
-        }
-      };
-
-      [startDateInput, endDateInput].forEach((date) => {
-        date.addEventListener("change", (event) => {
-          dynamicPrice();
-        });
-      });
-
-      const clear = document.getElementById("clear_button");
-      clear.addEventListener(
-        "click",
-        function () {
-          picker.clear();
-          totalDays.innerText = 0;
-          stringDays.innerText = " days";
-          totalCostElement.innerText = 0;
-        },
-        false
-      );
-    }
+    const clear = document.getElementById("clear_button");
+    clear.addEventListener(
+      "click",
+      function () {
+        picker.clear();
+        totalDays.innerText = 0;
+        stringDays.innerText = " days";
+        totalCostElement.innerText = 0;
+      },
+      false
+    );
   }
 }
