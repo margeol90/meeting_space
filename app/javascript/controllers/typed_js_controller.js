b06e87b7cb32a7cf353b3aefc4c7b4c7b8266434
Margarita Dagla <margaritadagla@hotmail.com>
2023-03-30 19:33:15 +0100

    speed altered

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,9 +5,11 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Book meeting spaces <strong><i>anytime</i></strong>", "Book meeting spaces <strong><i>anywhere</i></strong>...",],
-      typeSpeed: 25,
-      backspeed: 25,
+      strings: ["Book meeting spaces <strong><i>anytime</i></strong>", "Book meeting spaces <strong><i>anywhere</i></strong>",],
+      typeSpeed: 60,
+      startDelay: 300,
+      backspeed: 300,
+      backDelay: 400,
       loop: true
     })
   }
